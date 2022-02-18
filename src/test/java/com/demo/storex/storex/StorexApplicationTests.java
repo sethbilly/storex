20448cbdd0675eeecc4f4bb54c894449a4431be6
Tema Methodist Diocese <sethbilly.nartey@gmail.com>
2022-02-18 22:38:21 +0000

    Initial commit of initialized API project

---
+++ src/test/java/com/demo/storex/storex/StorexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.storex.storex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
